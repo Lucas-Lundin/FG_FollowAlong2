a24393173968b983e9da811c08b1a54167c2f2b9
Lucas-Lundin <113420344+Lucas-Lundin@users.noreply.github.com>
2022-09-13 14:37:24 +0200

    Triggers, Controlls

---
+++ FG_FollowAlong2_proj/Assets/Scripts/ControllsWorldPosition.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllsWorldPosition : MonoBehaviour
+{
+    [SerializeField] private Rigidbody characterBody;
+    [SerializeField] private float speed = 5f;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetAxis("Horizontal") != 0)
+        {
+            transform.Translate(transform.right * speed * Time.deltaTime * (Input.GetAxis("Horizontal")));
+        }
+
+        if (Input.GetAxis("Vertical") != 0)
+        {
+            transform.Translate(transform.forward * speed * Time.deltaTime * (Input.GetAxis("Vertical")));
+        }
+
+
+
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
+
+
+
+    }
+
+    private void Jump()
+    {
+        characterBody.velocity = Vector3.up * 5f;
+    }
+
+}
